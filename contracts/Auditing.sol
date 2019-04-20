1dc7357dc6ce6e573d779410bc99e362c00c32a7
David <djsfdavid@gmail.com>
2019-04-20 12:12:04 +0200

    set up auditing contract

---
+++ contracts/Auditing.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.5.0;
+
+contract Auditing{
+    uint public validatorCount = 0;
+/*
+   struct Validator{
+       uint id;
+       uint stake;
+       bool valid;
+   }
+   
+   mapping(uint => Validator) public validators;
+   
+   constructor() public{
+       createValidator(10);
+   }
+   
+   function createValidator(uint _stake) public{
+       validatorCount++;
+       validators[validatorCount] = Validator(validatorCount, _stake, false);
+       //Trigger event
+   }  
+*/
+}
