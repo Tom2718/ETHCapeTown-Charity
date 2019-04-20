03539fb1802a00c545ff1a1e12cc8d1dc775d62f
Thomas Tumiel <ttumiel@gmail.com>
2019-04-20 18:04:13 +0200

    uncomment code

--- contracts/Auditing.sol
+++ contracts/Auditing.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.5.0;
 
 contract Auditing{
     uint public validatorCount = 0;
-/*
+
    struct Validator{
        uint id;
        uint stake;
@@ -19,6 +19,5 @@ contract Auditing{
        validatorCount++;
        validators[validatorCount] = Validator(validatorCount, _stake, false);
        //Trigger event
-   }  
-*/
+   } 
 }
